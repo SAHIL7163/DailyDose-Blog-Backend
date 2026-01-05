420d08da8a88b7f3ee7d330e09c2d5d266d00aae
sahil <sahil@asqi.in>
2026-01-06 01:40:28 +0530

    Add BACKEND_URL to allowedOrigins for improved configuration flexibility

--- Config/allowedOrigins.js
+++ Config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "https://www.google.com",
   process.env.FRONTEND_URL || "http://localhost:3000",
+  process.env.BACKEND_URL || "http://localhost:8000",
 ];
 
 module.exports = allowedOrigins;
